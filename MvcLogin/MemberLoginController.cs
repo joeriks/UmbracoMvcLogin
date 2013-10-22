1ce5a619e3299345818c3094e2d7046217e2f0c2
Jonas Eriksson <jonas@programenta.se>
2013-10-22 18:26:47 +0200

    initial commit

---
+++ MvcLogin/MemberLoginController.cs
@@ -0,0 +1,59 @@
+﻿using MvcLogin;
+using System;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+
+    //I could not get plugincontroller to work [PluginController("MvcLogin")]
+    public class MemberLoginController : Umbraco.Web.Mvc.SurfaceController
+    {
+
+        // The MemberLogin Action returns the view, which we will create later. It also instantiates a new, empty model for our view:
+
+        [HttpGet]
+        [ActionName("MemberLogin")]
+        public ActionResult MemberLoginGet()
+        {
+            var model = new MemberLoginModel { ReturnUrl = Request.Url.AbsoluteUri };
+            return PartialView("MemberLogin", model);
+        }
+
+        // The MemberLogout Action signs out the user and redirects to the site home page:
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MemberLogout()
+        {
+            TempData["MemberLoginMessage"] = umbraco.library.GetDictionaryItem("You have been logged out");
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        // The MemberLoginPost Action checks the entered credentials using the standard Asp Net membership provider and redirects the user to the same page. Either as logged in, or with a message set in the TempData dictionary:
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("MemberLogin")]
+        public ActionResult MemberLoginPost(MemberLoginModel model)
+        {
+            if (Membership.ValidateUser(model.Username, model.Password))
+            {
+
+                FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
+
+                if (!string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    var redirect = new Uri(model.ReturnUrl);
+                    if (redirect.Host == Request.Url.Host) return Redirect(model.ReturnUrl);
+                }
+                return RedirectToCurrentUmbracoPage();
+
+            }
+            else
+            {
+                TempData["MemberLoginMessage"] = umbraco.library.GetDictionaryItem("Invalid username or password");
+                return RedirectToCurrentUmbracoPage();
+            }
+        }
+    }
---
+++ MvcLogin/MemberLoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcLogin
+{
+    public class MemberLoginModel
+    {
+        [ScaffoldColumn(false)]
+        public string ReturnUrl { get; set; }
+
+        [Required, Display(Name = "Enter your username")]
+        public string Username { get; set; }
+
+        [Required, Display(Name = "Password"), DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
